054e97d877956c87b0e6c328966705549d48f2e5
ali abbani <aliox@ahlam.com>
2021-08-09 16:36:42 +0300

    add the local strorage

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 let bookList = [
-  {
-    title: 'Nature',
-    author: 'Ali',
-  },
-  {
-    title: 'The Earth',
-    author: 'Henry',
-  },
+ 
 ];
 
 const addNewBook = () => {
@@ -17,15 +10,17 @@ const addNewBook = () => {
     author,
   };
   bookList.push(book);
+  saveDataLocally();
+  generateBooks();
 };
 
 const removeBook = (index) => {
   bookList = bookList.filter((book, ind) => ind !== index);
+  saveDataLocally();
   generateBooks();
 };
 
 const listShowContainer = document.querySelector('.listShow');
-
 
 const generateBooks = () => {
   listShowContainer.innerHTML = '';
@@ -37,17 +32,17 @@ const generateBooks = () => {
     titleSpan.className = 'book-title';
     titleSpan.textContent = bookObject.title;
     div.appendChild(titleSpan);
-    
+
     const br = document.createElement('br');
     div.appendChild(br);
-    
+
     const authorSpan = document.createElement('span');
     authorSpan.className = 'book-author';
     authorSpan.textContent = bookObject.author;
     div.appendChild(authorSpan);
     const br2 = document.createElement('br');
     div.appendChild(br2);
-    
+
     const removeButton = document.createElement('button');
     removeButton.className = 'remove-button';
     removeButton.textContent = 'Remove';
@@ -56,13 +51,27 @@ const generateBooks = () => {
       console.log(bookList);
     });
     div.appendChild(removeButton);
-  
+
     const hr = document.createElement('hr');
     hr.className = 'rule';
     div.appendChild(hr);
-  
+
     listShowContainer.appendChild(div);
   });
-}
+};
 
 generateBooks();
+
+if(localStorage.getItem('bookList')) {
+    const localBookList = localStorage.getItem('bookList');
+    const convertedBookList = JSON.parse(localBookList);
+    bookList = convertedBookList;
+}
+
+const addButton = document.querySelector('.add');
+addButton.addEventListener('click', addNewBook);
+
+const saveDataLocally = () => {
+    const stringifiedBookList = JSON.stringify(bookList);
+    localStorage.setItem('bookList', stringifiedBookList);
+}
