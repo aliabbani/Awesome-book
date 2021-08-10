e14218a25e012c575e1b04e91f31d9ea2d6742aa
ali abbani <aliox@ahlam.com>
2021-08-10 15:50:33 +0300

    refactoring the index js file to use classes inside it

--- index.js
+++ index.js
@@ -1,55 +1,127 @@
-let bookList = [];
+class Booklet {
+  constructor(title, author, listShowContainer) {
+    this.bookList = [];
+    this.title = title;
+    this.author = author;
+    this.listShowContainer = listShowContainer;
+  }
 
-const saveDataLocally = (bookList) => {
-  const stringifiedBookList = JSON.stringify(bookList);
-  localStorage.setItem('bookList', stringifiedBookList);
-};
+  removeBook = (index) => {
+    this.bookList = this.bookList.filter((book, ind) => ind !== index);
+  };
 
-const removeBook = (index) => {
-  bookList = bookList.filter((book, ind) => ind !== index);
-};
+  addNewBook = (bookList) => {
+    const title = document.querySelector('.title').value;
+    const author = document.querySelector('.author').value;
+    const book = {
+      title,
+      author,
+    };
+    this.bookList.push(book);
+    this.saveDataLocally(bookList);
+    this.generateBooks();
+  };
 
-const listShowContainer = document.querySelector('.listShow');
+  generateBooks = () => {
+    this.listShowContainer = '';
 
-const generateBooks = () => {
-  listShowContainer.innerHTML = '';
+    this.bookList.reverse().forEach((bookObject, index) => {
+      const div = document.createElement('div');
+      div.className = 'book';
 
-  bookList.reverse().forEach((bookObject, index) => {
-    const div = document.createElement('div');
-    div.className = 'book';
+      const titleSpan = document.createElement('span');
+      titleSpan.className = 'book-title';
+      titleSpan.textContent = bookObject.title;
+      div.appendChild(titleSpan);
 
-    const titleSpan = document.createElement('span');
-    titleSpan.className = 'book-title';
-    titleSpan.textContent = bookObject.title;
-    div.appendChild(titleSpan);
+      const br = document.createElement('br');
+      div.appendChild(br);
 
-    const br = document.createElement('br');
-    div.appendChild(br);
+      const authorSpan = document.createElement('span');
+      authorSpan.className = 'book-author';
+      authorSpan.textContent = bookObject.author;
+      div.appendChild(authorSpan);
+      const br2 = document.createElement('br');
+      div.appendChild(br2);
 
-    const authorSpan = document.createElement('span');
-    authorSpan.className = 'book-author';
-    authorSpan.textContent = bookObject.author;
-    div.appendChild(authorSpan);
-    const br2 = document.createElement('br');
-    div.appendChild(br2);
+      const removeButton = document.createElement('button');
+      removeButton.className = 'remove-button';
+      removeButton.textContent = 'Remove';
+      removeButton.addEventListener('click', () => {
+        this.removeBook(index);
+        this.saveDataLocally(this.bookList);
+        this.generateBooks();
+      });
+      div.appendChild(removeButton);
 
-    const removeButton = document.createElement('button');
-    removeButton.className = 'remove-button';
-    removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', () => {
-      removeBook(index);
-      saveDataLocally(bookList);
-      generateBooks();
+      const hr = document.createElement('hr');
+      hr.className = 'rule';
+      div.appendChild(hr);
+
+      this.listShowContainer.appendChild(div);
     });
-    div.appendChild(removeButton);
+  };
 
-    const hr = document.createElement('hr');
-    hr.className = 'rule';
-    div.appendChild(hr);
+  saveDataLocally = (bookList) => {
+    const stringifiedBookList = JSON.stringify(bookList);
+    localStorage.setItem('bookList', stringifiedBookList);
+  };
+}
 
-    listShowContainer.appendChild(div);
-  });
-};
+// let bookList = [];
+
+// const saveDataLocally = (bookList) => {
+//   const stringifiedBookList = JSON.stringify(bookList);
+//   localStorage.setItem('bookList', stringifiedBookList);
+// };
+
+// const removeBook = (index) => {
+//   bookList = bookList.filter((book, ind) => ind !== index);
+// };
+
+// const listShowContainer = document.querySelector('.listShow');
+
+// const generateBooks = () => {
+//   listShowContainer.innerHTML = '';
+
+//   bookList.reverse().forEach((bookObject, index) => {
+//     const div = document.createElement('div');
+//     div.className = 'book';
+
+//     const titleSpan = document.createElement('span');
+//     titleSpan.className = 'book-title';
+//     titleSpan.textContent = bookObject.title;
+//     div.appendChild(titleSpan);
+
+//     const br = document.createElement('br');
+//     div.appendChild(br);
+
+//     const authorSpan = document.createElement('span');
+//     authorSpan.className = 'book-author';
+//     authorSpan.textContent = bookObject.author;
+//     div.appendChild(authorSpan);
+//     const br2 = document.createElement('br');
+//     div.appendChild(br2);
+
+//     const removeButton = document.createElement('button');
+//     removeButton.className = 'remove-button';
+//     removeButton.textContent = 'Remove';
+//     removeButton.addEventListener('click', () => {
+//       removeBook(index);
+//       saveDataLocally(bookList);
+//       generateBooks();
+//     });
+//     div.appendChild(removeButton);
+
+//     const hr = document.createElement('hr');
+//     hr.className = 'rule';
+//     div.appendChild(hr);
+
+//     listShowContainer.appendChild(div);
+//   });
+// };
+
+
 
 if (localStorage.getItem('bookList') !== null) {
   const localBookList = localStorage.getItem('bookList');
@@ -60,17 +132,17 @@ if (localStorage.getItem('bookList') !== null) {
   generateBooks();
 }
 
-const addNewBook = (bookList) => {
-  const title = document.querySelector('.title').value;
-  const author = document.querySelector('.author').value;
-  const book = {
-    title,
-    author,
-  };
-  bookList.push(book);
-  saveDataLocally(bookList);
-  generateBooks();
-};
+// const addNewBook = (bookList) => {
+//   const title = document.querySelector('.title').value;
+//   const author = document.querySelector('.author').value;
+//   const book = {
+//     title,
+//     author,
+//   };
+//   bookList.push(book);
+//   saveDataLocally(bookList);
+//   generateBooks();
+// };
 
 const addButton = document.querySelector('.add');
 addButton.addEventListener('click', () => addNewBook(bookList));
