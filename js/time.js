31ad70c741114f004bf2cd563e8fa6d58d127e7f
HENRY-KC <henrykc24@yahoo.com>
2021-08-12 16:56:23 +0100

    Remove ternary operators

--- js/time.js
+++ js/time.js
@@ -1,29 +1,33 @@
 const getTime = () => {
   const getOrdinal = (param) => {
     param = Number(param).toString();
-    const splitPop = param.split('').pop();
-    const splitShift = param.split('').shift();
+    const splitPop = param.split("").pop();
+    const splitShift = param.split("").shift();
     const len = param.length;
-    return splitPop === '1' && len === 1
-      ? 'st'
-      : len === 2
-        && splitPop === '1'
-        && (splitShift === '2' || splitShift === '3')
-        ? 'st'
-        : splitPop === '2' && len === 1
-          ? 'nd'
-          : len === 2 && splitPop === '2' && splitShift === '2'
-            ? 'nd'
-            : splitPop === '3' && len === 1
-              ? 'rd'
-              : len === 2 && splitPop === '3' && splitShift === '2'
-                ? 'rd'
-                : 'th';
+    if (splitPop === "1" && len === 1) {
+      return "st";
+    } else if (
+      len === 2 &&
+      splitPop === "1" &&
+      (splitShift === "2" || splitShift === "3")
+    ) {
+      return "st";
+    } else if (splitPop === "2" && len === 1) {
+      return "nd";
+    } else if (len === 2 && splitPop === "2" && splitShift === "2") {
+      return "nd";
+    } else if (splitPop === "3" && len === 1) {
+      return "rd";
+    } else if (len === 2 && splitPop === "3" && splitShift === "2") {
+      return "rd";
+    }
+
+    return "th";
   };
   const amPm = (hr) => {
-    let pmAm = 'pm';
+    let pmAm = "pm";
     if (hr < 12) {
-      pmAm = 'am';
+      pmAm = "am";
     }
     return pmAm;
   };
@@ -32,28 +36,17 @@ const getTime = () => {
   const dt = DateTime.now();
   const x = dt.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
 
-  let [day, month, year, time] = x.split(' ');
+  let [day, month, year, time] = x.split(" ");
 
-  let [hr, min, sec] = time.split(':');
+  let [hr, min, sec] = time.split(":");
   hr = hr > 11 ? hr - 12 : hr;
   time = `${hr}:${min}:${sec}`;
 
-  const wantedTimeDate = `${
-    month
-  } ${
-    day
-  }${getOrdinal(day.toString())
-  } ${
-    year
-  } ${
-    time
-  } ${
-    amPm(day)}`;
+  const wantedTimeDate = `${month} ${day}${getOrdinal(
+    day.toString()
+  )} ${year} ${time} ${amPm(day)}`;
   return wantedTimeDate;
 };
-setInterval(
-  () => {
-    (document.querySelector('.time-date').textContent = getTime());
-  },
-  1000,
-);
+setInterval(() => {
+  document.querySelector(".time-date").textContent = getTime();
+}, 1000);
