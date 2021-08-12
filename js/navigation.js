09e93fd9170359d16319d50fbc4c449a8abd83fe
ali abbani <aliox@ahlam.com>
2021-08-12 18:51:01 +0300

    Add and change the content of the page

--- js/navigation.js
+++ js/navigation.js
@@ -6,6 +6,8 @@ const book = document.querySelector('.all-books');
 const addBooks = document.querySelector('.add-books');
 const contactBox = document.querySelector('.contact-info');
 
+const listShow1 = document.querySelector('.listShow')
+
 listNav.addEventListener('click', () => {
   addBooks.style.display = 'none';
   contactBox.style.display = 'none';
@@ -23,3 +25,9 @@ contactNav.addEventListener('click', () => {
   book.style.display = 'none';
   contactBox.style.display = 'flex';
 });
+
+if (listShow1.innerHTML === '') {
+  document.querySelector('.all-books').classList.add('hide');
+} else {
+  document.querySelector('.all-books').classList.remove('hide');
+}
--- js/time.js
+++ js/time.js
@@ -1,34 +1,34 @@
 const getTime = () => {
   const getOrdinal = (param) => {
     param = Number(param).toString();
-    const splitPop = param.split("").pop();
-    const splitShift = param.split("").shift();
+    const splitPop = param.split('').pop();
+    const splitShift = param.split('').shift();
     const len = param.length;
-    return splitPop === "1" && len === 1
-      ? "st"
-      : len === 2 &&
-        splitPop === "1" &&
-        (splitShift === "2" || splitShift === "3")
-      ? "st"
-      : splitPop === "2" && len === 1
-      ? "nd"
-      : len === 2 && splitPop === "2" && splitShift === "2"
-      ? "nd"
-      : splitPop === "3" && len === 1
-      ? "rd"
-      : len === 2 && splitPop === "3" && splitShift === "2"
-      ? "rd"
-      : "th";
+    return splitPop === '1' && len === 1
+      ? 'st'
+      : len === 2
+        && splitPop === '1'
+        && (splitShift === '2' || splitShift === '3')
+        ? 'st'
+        : splitPop === '2' && len === 1
+          ? 'nd'
+          : len === 2 && splitPop === '2' && splitShift === '2'
+            ? 'nd'
+            : splitPop === '3' && len === 1
+              ? 'rd'
+              : len === 2 && splitPop === '3' && splitShift === '2'
+                ? 'rd'
+                : 'th';
   };
   const amPm = (hr) => {
-    let pmAm = "pm";
+    let pmAm = 'pm';
     if (hr < 12) {
-      pmAm = "am";
+      pmAm = 'am';
     }
     return pmAm;
   };
 
-  let DateTime = luxon.DateTime;
+  const { DateTime } = luxon;
   const dt = DateTime.now();
   const x = dt.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
 
@@ -36,12 +36,24 @@ const getTime = () => {
 
   let [hr, min, sec] = time.split(':');
   hr = hr > 11 ? hr - 12 : hr;
-  time = '' + hr + ':' + min + ':' + sec;
+  time = `${hr}:${min}:${sec}`;
 
-  const wantedTimeDate = '' + month + ' ' + day + getOrdinal(day.toString()) + ' ' + year + ' ' + time + ' ' + amPm(day);
+  const wantedTimeDate = `${
+    month
+  } ${
+    day
+  }${getOrdinal(day.toString())
+  } ${
+    year
+  } ${
+    time
+  } ${
+    amPm(day)}`;
   return wantedTimeDate;
 };
 setInterval(
-  () => (document.querySelector(".time-date").textContent = getTime()),
-  1000
+  () => {
+    (document.querySelector('.time-date').textContent = getTime());
+  },
+  1000,
 );
