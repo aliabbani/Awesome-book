52ace89f873ec90c38decf7c1f64a56e188b1051
HENRY-KC <henrykc24@yahoo.com>
2021-08-12 15:41:22 +0100

    Resolve variable name conflict

--- js/navigation.js
+++ js/navigation.js
@@ -2,24 +2,24 @@ const listNav = document.querySelector('.list');
 const addNewNav = document.querySelector('.add-new');
 const contactNav = document.querySelector('.contact');
 
-const allBooks = document.querySelector('.all-books');
+const book = document.querySelector('.all-books');
 const addBooks = document.querySelector('.add-books');
 const contactBox = document.querySelector('.contact-info');
 
 listNav.addEventListener('click', () => {
   addBooks.style.display = 'none';
   contactBox.style.display = 'none';
-  allBooks.style.display = 'block';
+  book.style.display = 'block';
 });
 
 addNewNav.addEventListener('click', () => {
-  allBooks.style.display = 'none';
+  book.style.display = 'none';
   contactBox.style.display = 'none';
   addBooks.style.display = 'flex';
 });
 
 contactNav.addEventListener('click', () => {
   addBooks.style.display = 'none';
-  allBooks.style.display = 'none';
+  book.style.display = 'none';
   contactBox.style.display = 'flex';
 });
