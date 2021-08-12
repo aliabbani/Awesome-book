c62efd534e849e5844418e2dc241d42d17939bbb
ali abbani <aliox@ahlam.com>
2021-08-13 00:34:50 +0300

    Add time display

--- js/time.js
+++ js/time.js
@@ -1,64 +1,11 @@
-const getTime = () => {
-  const getOrdinal = (param) => {
-    param = Number(param).toString();
-    const splitPop = param.split('').pop();
-    const splitShift = param.split('').shift();
-    const len = param.length;
-    if (splitPop === '1' && len === 1) {
-      return 'st';
-    } if (
-      len === 2
-      && splitPop === '1'
-      && (splitShift === '2' || splitShift === '3')
-    ) {
-      return 'st';
-    } if (splitPop === '2' && len === 1) {
-      return 'nd';
-    } if (len === 2 && splitPop === '2' && splitShift === '2') {
-      return 'nd';
-    } if (splitPop === '3' && len === 1) {
-      return 'rd';
-    } if (len === 2 && splitPop === '3' && splitShift === '2') {
-      return 'rd';
-    }
-
-    return 'th';
-  };
-  const amPm = (hr) => {
-    let pmAm = 'pm';
-    if (hr < 12) {
-      pmAm = 'am';
-    }
-    return pmAm;
-  };
-
-  const { DateTime } = luxon;
-  const dt = DateTime.now();
-  const x = dt.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
-
-  let [day, month, , time] = x.split(' ');
-  const [, , year] = x.split(' ');
-
-  const temp = day;
-  if (day.length > 2) {
-    month = temp;
-  }
-
-  const [, min, sec] = time.split(':');
-  let [hr] = time.split(':');
-  hr = hr > 11 ? hr - 12 : hr;
-  time = `${hr}:${min}:${sec}`;
-  if (month.length === 3) {
-    month = month.replace(/,/g, '');
-  }
-  if (day.length === 3) {
-    day = day.replace(/,/g, '');
-  }
-  const wantedTimeDate = `${month} ${day}${getOrdinal(
-    day.toString(),
-  )} ${year} ${time} ${amPm(day)}`;
-  return wantedTimeDate;
+const getTimeAndDate = () => {
+  /* eslint-disable no-undef */
+  const dt = luxon.DateTime.now();
+  return dt.toLocaleString(luxon.DateTime.DATETIME_MED_WITH_SECONDS);
+  /* eslint-disable no-undef */
 };
-setInterval(() => {
-  document.querySelector('.time-date').textContent = getTime();
-}, 1000);
+
+setInterval(
+  () => { (document.querySelector('.time-date').textContent = getTimeAndDate()); },
+  1000,
+);
